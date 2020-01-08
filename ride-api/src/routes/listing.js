e4e56d4f70d5e77702d01f63ed19f9a01a18d647
sbentley2019 <sbentley000@gmail.com>
2020-01-08 17:02:26 -0500

    add put route for listing

--- ride-api/src/routes/listing.js
+++ ride-api/src/routes/listing.js
@@ -11,5 +11,31 @@ module.exports = db => {
     });
   });
 
+  router.put("/listing", (request, response) => {
+    if (process.env.TEST_ERROR) {
+      setTimeout(() => response.status(500).json({}), 1000);
+      return;
+    }
+    // need to add user_id and other listing variables into the db query
+    const {
+      make,
+      model,
+      year,
+      imageUrl,
+      isSold,
+      price,
+      kms,
+      city
+    } = request.body;
+
+    console.log(make, model, year, imageUrl, isSold, price, kms, city);
+    db.query(
+      `
+      INSERT INTO listing (make, model, year, listing_image, is_sold, price, kms, city) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+    `,
+      [make, model, year, imageUrl, isSold, price, kms, city]
+    );
+  });
+
   return router;
 };
