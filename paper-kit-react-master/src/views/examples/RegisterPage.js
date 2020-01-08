bf4a6adfe4628ae58c7b19eb4000170d33ec4d95
mwong01 <mattw90@gmail.com>
2020-01-08 11:54:35 -0500

    migrate to paper-kit-react

---
+++ paper-kit-react-master/src/views/examples/RegisterPage.js
@@ -0,0 +1,70 @@
+/*!
+
+=========================================================
+* Paper Kit React - v1.0.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/paper-kit-react
+
+* Copyright 2019 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/paper-kit-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+
+// reactstrap components
+import { Button, Card, Form, Input, Container, Row, Col } from "reactstrap";
+
+// core components
+import IndexNavbar from "components/Navbars/IndexNavbar.js";
+// import ExamplesNavbar from "components/Navbars/ExamplesNavbar.js";
+
+function RegisterPage() {
+  document.documentElement.classList.remove("nav-open");
+  React.useEffect(() => {
+    document.body.classList.add("register-page");
+    return function cleanup() {
+      document.body.classList.remove("register-page");
+    };
+  });
+  return (
+    <>
+      <IndexNavbar />
+      <div
+        className="page-header"
+        style={{
+          backgroundImage: "url(" + require("assets/img/5-audi-rs5.jpg") + ")"
+        }}
+      >
+        <div className="filter" />
+        <Container>
+          <Row>
+            <Col className="ml-auto mr-auto" lg="4">
+              <Card className="card-register ml-auto mr-auto">
+                <h2 className="title mx-auto">Register Below</h2>
+                <Form className="register-form">
+                  <label>Email</label>
+                  <Input placeholder="Email" type="text" />
+                  <label>Password</label>
+                  <Input placeholder="Password" type="password" />
+                  <Button block className="btn-round" color="danger">
+                    Register
+                  </Button>
+                </Form>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+       
+      </div>
+    </>
+  );
+}
+
+export default RegisterPage;
---
+++ paper-kit-react-master/src/views/index-sections/SectionCarousel.js
@@ -0,0 +1,152 @@
+/*!
+
+=========================================================
+* Paper Kit React - v1.0.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/paper-kit-react
+
+* Copyright 2019 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/paper-kit-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+
+// reactstrap components
+import {
+  Card,
+  Container,
+  Row,
+  Col,
+  Carousel,
+  CarouselItem,
+  CarouselIndicators,
+  CarouselCaption
+} from "reactstrap";
+
+// core components
+
+const items = [
+  {
+    src: require("assets/img/1-sti-hyperblue.jpg"),
+    altText: "2016Subaru STI",
+    caption: "2016 Subaru STI"
+  },
+  {
+    src: require("assets/img/2-crosstrek.PNG"),
+    altText: "2019 Subaru Crosstrek",
+    caption: "2019 Subaru Crosstrek"
+  },
+  {
+    src: require("assets/img/3-shitty-accord.jpg"),
+    altText: "2002 Honda Accord",
+    caption: "2002 Honda Accord"
+  },
+  {
+    src: require("assets/img/4-911-gt3.jpg"),
+    altText: "2017 Porsche 911 GT3",
+    caption: "2017 Porsche 911 GT3"
+  }
+];
+
+function SectionCarousel() {
+  const [activeIndex, setActiveIndex] = React.useState(0);
+  const [animating, setAnimating] = React.useState(false);
+  const onExiting = () => {
+    setAnimating(true);
+  };
+  const onExited = () => {
+    setAnimating(false);
+  };
+  const next = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
+    setActiveIndex(nextIndex);
+  };
+  const previous = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
+    setActiveIndex(nextIndex);
+  };
+  const goToIndex = newIndex => {
+    if (animating) return;
+    setActiveIndex(newIndex);
+  };
+  return (
+    <>
+      <div className="section pt-o" id="carousel">
+        <Container>
+        <div className="title">
+            <h2>Featured rides of the day:</h2>
+          </div>
+          <Row>
+            <Col className="ml-auto mr-auto" md="8">
+              <Card className="page-carousel">
+                <Carousel
+                  activeIndex={activeIndex}
+                  next={next}
+                  previous={previous}
+                >
+                  <CarouselIndicators
+                    items={items}
+                    activeIndex={activeIndex}
+                    onClickHandler={goToIndex}
+                  />
+                  {items.map(item => {
+                    return (
+                      <CarouselItem
+                        onExiting={onExiting}
+                        onExited={onExited}
+                        key={item.src}
+                      >
+                        <img src={item.src} alt={item.altText} />
+                        <CarouselCaption
+                          captionText={item.caption}
+                          captionHeader=""
+                        />
+                      </CarouselItem>
+                    );
+                  })}
+                  <a
+                    className="left carousel-control carousel-control-prev"
+                    data-slide="prev"
+                    href="#pablo"
+                    onClick={e => {
+                      e.preventDefault();
+                      previous();
+                    }}
+                    role="button"
+                  >
+                    <span className="fa fa-angle-left" />
+                    <span className="sr-only">Previous</span>
+                  </a>
+                  <a
+                    className="right carousel-control carousel-control-next"
+                    data-slide="next"
+                    href="#pablo"
+                    onClick={e => {
+                      e.preventDefault();
+                      next();
+                    }}
+                    role="button"
+                  >
+                    <span className="fa fa-angle-right" />
+                    <span className="sr-only">Next</span>
+                  </a>
+                </Carousel>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+      </div>{" "}
+    </>
+  );
+}
+
+export default SectionCarousel;
