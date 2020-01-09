c46c8728ca810b4b2c45c9a68c34bc610d2a93a2
sbentley2019 <sbentley000@gmail.com>
2020-01-09 13:02:57 -0500

    Add multiple routes for listings

--- ride-api/src/routes/listing.js
+++ ride-api/src/routes/listing.js
@@ -1,6 +1,27 @@
 const router = require("express").Router();
 
 module.exports = db => {
+  router.get("/listing/make", (request, response) => {
+    db.query(
+      `
+      SELECT DISTINCT make FROM listing
+      `
+    ).then(data => {
+      response.json(data.rows);
+    });
+  });
+
+  router.get("/listing/make/:id", (request, response) => {
+    console.log("got here");
+    db.query(
+      `
+      SELECT DISTINCT model FROM listing WHERE make = $1`,
+      [request.params.id]
+    ).then(data => {
+      response.json(data.rows);
+    });
+  });
+
   router.get("/listing", (request, response) => {
     db.query(
       `
@@ -11,12 +32,12 @@ module.exports = db => {
     });
   });
 
-  router.put("/listing", (request, response) => {
+  router.put("/listing/:id", (request, response) => {
     if (process.env.TEST_ERROR) {
       setTimeout(() => response.status(500).json({}), 1000);
       return;
     }
-    // need to add user_id and other listing variables into the db query
+    // need to add other listing variables into the db query
     const {
       make,
       model,
@@ -31,10 +52,32 @@ module.exports = db => {
     console.log(make, model, year, imageUrl, isSold, price, kms, city);
     db.query(
       `
-      INSERT INTO listing (make, model, year, listing_image, is_sold, price, kms, city) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+      INSERT INTO listing (make, model, year, user_id, listing_image, is_sold, price, kms, city) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
     `,
-      [make, model, year, imageUrl, isSold, price, kms, city]
+      [make, model, year, request.params.id, imageUrl, isSold, price, kms, city]
     );
+  });
+
+  router.get("/listing/:id", (request, response) => {
+    db.query(
+      `
+      SELECT * FROM listing WHERE id = $1::integer
+      `,
+      [request.params.id]
+    ).then(data => {
+      response.json(data.rows);
+    });
+  });
+
+  router.get("/listing/user/:id", (request, response) => {
+    db.query(
+      `
+      SELECT * FROM listing WHERE user_id = $1
+      `,
+      [request.params.id]
+    ).then(data => {
+      response.json(data.rows);
+    });
   });
 
   return router;
