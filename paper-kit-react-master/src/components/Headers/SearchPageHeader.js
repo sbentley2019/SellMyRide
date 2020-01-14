16e21faba16ace36a20841159095343e46da6f98
sbentley2019 <sbentley000@gmail.com>
2020-01-14 15:58:39 -0500

    Change search dropdowns so they are ordered

--- paper-kit-react-master/src/components/Headers/SearchPageHeader.js
+++ paper-kit-react-master/src/components/Headers/SearchPageHeader.js
@@ -69,7 +69,7 @@ function SearchPageHeader(props) {
 
   const sendMake = function(e) {
     setState({ ...state, make: e, model: "" });
-    axios.get(`/api/listing/make/${e}`).then(res => {
+    axios.get(`/api/listing/findModel/${e}`).then(res => {
       setModelArr(res.data.map(listing => listing.model));
     });
   };
