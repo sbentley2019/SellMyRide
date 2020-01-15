349e542d3ed9377fbc60bab80e18224df48098da
sbentley2019 <sbentley000@gmail.com>
2020-01-15 10:35:56 -0500

    Add message feature on listing page

--- paper-kit-react-master/src/views/examples/VehicleListing.js
+++ paper-kit-react-master/src/views/examples/VehicleListing.js
@@ -15,6 +15,8 @@ import {
   CardTitle,
   Form,
   Input,
+  Modal,
+  FormGroup,
   InputGroupAddon,
   InputGroupText,
   InputGroup,
@@ -75,6 +77,8 @@ export default function VehicleListing() {
   document.documentElement.classList.remove("nav-open");
   const [cookies, setCookie] = useCookies(["name", "user_id"]);
   const [vehicle, setVehicle] = useState({});
+  const [liveModal, setLiveModal] = useState(false);
+  const [message, setMessage] = useState("");
   useEffect(() => {
     document.body.classList.add("profile-page");
     return function cleanup() {
@@ -91,13 +95,82 @@ export default function VehicleListing() {
   postal = vehicle.city;
   car = vehicle;
 
+  const sendText = function() {
+    console.log("send text");
+    if (!cookies.user_id) {
+      alert("Register or login to send messages.");
+    }
+    if (message) {
+      axios.put(`/api/messages/${cookies.user_id}/${vehicle.user_id}`, {
+        message: message
+      });
+      setLiveModal(false);
+    }
+  };
   /*   console.log("props:", ); */
   return (
     <>
       <IndexNavbar />
       <NewVehicleListingHeader data={vehicle} />
       <VehicleProfileCarousel data={vehicle} />
-      <VehicleProfileDescription data={vehicle} />
+      <VehicleProfileDescription
+        fire={() => setLiveModal(true)}
+        data={vehicle}
+      />
+      <Modal isOpen={liveModal} toggle={() => setLiveModal(false)}>
+        <div className="modal-header">
+          <h5 className="modal-title" id="exampleModalLiveLabel">
+            Send Message
+          </h5>
+        </div>
+        <div className="modal-body">
+          <FormGroup row>
+            <Input
+              type="text"
+              min="0"
+              step="1"
+              name="text"
+              id="formText"
+              value={message}
+              onChange={e => {
+                setMessage(e.target.value);
+              }}
+            />
+          </FormGroup>
+        </div>
+        <div className="modal-footer">
+          <div className="left-side">
+            <Button
+              className="btn-link"
+              color="default"
+              data-dismiss="modal"
+              type="button"
+              onClick={() => setLiveModal(false)}
+            >
+              Never mind
+            </Button>
+          </div>
+          <div className="divider" />
+          <div className="right-side">
+            <Button
+              className="btn-link"
+              color="danger"
+              type="button"
+              onClick={() => sendText()}
+            >
+              Send
+            </Button>
+          </div>
+        </div>
+      </Modal>
+      {/* <Button
+        className="btn-round"
+        color="success"
+        outline
+        onClick={() => setLiveModal(true)}
+      >
+        Message Seller
+      </Button> */}
       <div style={{ width: "100vw", height: "100vh" }}>
         <WrappedMap
           googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${googleKey}`}
